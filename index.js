a129eb614d0c661d0a350279a298e403cafe699b
Harshit Sandilya <2021ucp1382@mnit.ac.in>
2023-08-02 20:18:45 +0530

    mongo connection

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+mongoose.set("strictQuery", true);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ze6xi3i.mongodb.net/?retryWrites=true&w=majority`;
+mongoose
+	.connect(uri, {
+		waitQueueTimeoutMS: 5000,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		dbName: "Blogs",
+	})
+	.catch((err) => {
+		console.error(err.stack);
+		process.exit(1);
+	})
+	.then(() => {
+		app.listen(process.env.PORT, () => {
+			console.log(`Server is running on port ${process.env.PORT}`);
+		});
+	});
 
 app.use("/", (req, res) => {
-	res.status(200).json({ message: "Backend is working" });
-});
-
-app.listen(process.env.PORT, () => {
-	console.log(`Server is running on port ${process.env.PORT}`);
+	res.status(200).json({ message: "Backend is running" });
 });
