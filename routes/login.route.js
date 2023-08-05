c40371c9429e12deb4d258d4e2bcd937deb0c4fa
Harshit Sandilya <2021ucp1382@mnit.ac.in>
2023-08-05 13:48:01 +0530

    fix error: multiple responses

--- routes/login.route.js
+++ routes/login.route.js
@@ -11,6 +11,7 @@ router.route("/").post(async (req, res) => {
 	if (user && bcrypt.compareSync(password, user.password)) {
 		const token = jwt.sign({ email: user.email, id: user._id }, process.env.JWT_SECRET);
 		res.status(200).json({ message: "User Login Accepted", data: token });
+		return;
 	}
 
 	res.status(401).json({ message: "User Login Failed" });
